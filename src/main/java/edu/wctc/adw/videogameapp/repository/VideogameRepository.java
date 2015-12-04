2372db613fe4062bbd54899b1f0fe9752c7d3f91
awoodward8129 <awoodward@my.wctc.edu>
2015-12-04 15:50:19 -0600

    works!

--- src/main/java/edu/wctc/adw/videogameapp/repository/VideogameRepository.java
+++ src/main/java/edu/wctc/adw/videogameapp/repository/VideogameRepository.java
@@ -1,11 +1,10 @@
 package edu.wctc.adw.videogameapp.repository;
 
+import edu.wctc.adw.videogameapp.entity.Videogame;
 import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import edu.wctc.adw.videogameapp.entity.Videogame;
-
 /**
  *
  * @author jlombardo
